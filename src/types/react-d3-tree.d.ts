cc88264b775bd098f476af698be9a494553c85cb
Jordan Woo <jkwoo1234@gmail.com>
2025-06-10 15:43:49 -0400

    one more lint error

--- src/types/react-d3-tree.d.ts
+++ src/types/react-d3-tree.d.ts
@@ -3,7 +3,8 @@ declare module 'react-d3-tree' {
 
   interface TreeNodeDatum {
     name: string;
-    attributes?: Record<string, unknown>;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    attributes?: Record<string, any>;
     children?: TreeNodeDatum[];
   }
 
